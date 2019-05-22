3a2be3fe075b9cc0f1be0de9cac3a2237e73063d
chr0527 <1015670600@qq.com>
2019-05-22 14:44:48 +0800

    init

--- pro-dubbo/src/main/java/com/sdsoon/demo/CityServiceImpl.java
+++ pro-dubbo/src/main/java/com/sdsoon/demo/CityServiceImpl.java
@@ -18,10 +18,10 @@ public class CityServiceImpl implements CityService {
     public List<City> queryCity() {
 
         City city1 = new City(1, "纽约", "No.1");
-        City city2 = new City(1, "洛杉矶", "No.1");
-        City city3 = new City(1, ",迈阿密", "No.2");
-        City city4 = new City(1, "芝加哥", "No.2");
-        City city5 = new City(1, "华盛顿", "No.2");
+        City city2 = new City(2, "洛杉矶", "No.1");
+        City city3 = new City(3, ",迈阿密", "No.2");
+        City city4 = new City(4, "芝加哥", "No.2");
+        City city5 = new City(5, "华盛顿", "No.2");
 
         List<City> cities = new ArrayList<City>();
         cities.add(city1);
